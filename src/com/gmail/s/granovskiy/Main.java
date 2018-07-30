e47e07004aa97dc715d549972c40c093fb02e930
SviatoslavExpert <s.granovskiy@gmail.com>
2018-07-30 17:01:27 +0300

    Update

--- src/com/gmail/s/granovskiy/Main.java
+++ src/com/gmail/s/granovskiy/Main.java
@@ -7,8 +7,12 @@ public class Main {
         Triangle triangleOne = new Triangle(0.3, 0.4, 0.5);
         Triangle triangleTwo = new Triangle(0.7, 0.8, 0.9);
 
-        /*  calling the method calculatePrintTriangleArea for Triangle class exemplars  */
-        triangleOne.calculatePrintTriangleArea();
-        triangleTwo.calculatePrintTriangleArea();
+        /*  calling the method calculateTriangleArea for Triangle class exemplars and printing the result  */
+        System.out.println(triangleOne.calculateTriangleArea());
+        System.out.println(triangleTwo.calculateTriangleArea());
+
+        /*  Calling the toString() method to see the state of the objects  */
+        System.out.println(triangleOne);
+        System.out.println(triangleTwo);
     }
 }
--- src/com/gmail/s/granovskiy/Triangle.java
+++ src/com/gmail/s/granovskiy/Triangle.java
@@ -6,9 +6,12 @@ public class Triangle {
     private double b;
     private double c;
     private double p;
+    private double calculationResult;
 
     /* object constructor of Triangle class */
-    Triangle(double a, double b, double c){
+    /*  access modifier: public !!! */
+    public Triangle(double a, double b, double c){
+        super();         /*  constructor by default  */
         this.a = a;
         this.b = b;
         this.c = c;
@@ -47,8 +50,14 @@ public class Triangle {
         this.p = p;
     }
 
-    public void calculatePrintTriangleArea(){
+    /*  method data type: double, not void!!!  The method returns triangle area. */
+    public double calculateTriangleArea(){
         p = (a + b + c)/2.0;
-        System.out.println("Площадь треугольника: " + Math.sqrt(p*(p - a)*(p - b)*(p - c)));
+        calculationResult = Math.sqrt(p*(p - a)*(p - b)*(p - c));
+        return calculationResult;
+    }
+    /*  The toString() method returns the string representation of the object.  */
+    public String toString() {
+        return "Triangle [side a=" + a + ", side b=" + b + ", side c=" + c + ",  p=" + p + ", calculationResult=" + calculationResult + "]";
     }
 }
